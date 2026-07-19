05661a095f8e87dc642817cea4dee6aafedf017d
Soban Syeed <Soban.syeed@outlook.com>
2026-07-20 03:52:35 +0530

    Add unit animations and movement

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] Vector3 targetPosition;
     [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotateSpeed = 10f;
     [SerializeField] float stoppingDistance = 0.1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,11 +18,23 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Move(MouseWorld.GetPosition());
         }
     }
 
