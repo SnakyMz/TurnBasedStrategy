203c51362197bd6c064a9ddb6e32b501b0372479
Soban Syeed <Soban.syeed@outlook.com>
2026-07-20 02:30:43 +0530

    Add Unit script

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float stoppingDistance = 0.1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
     }
 }
